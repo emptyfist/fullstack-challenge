ee3d67b12abc81dbb9830657c141b58386d2736a
EmptyFist <beauty.daughter0310@gmail.com>
2023-03-01 11:39:13 +0200

    Get weather info via Internal API

--- api/routes/api.php
+++ api/routes/api.php
@@ -20,3 +20,30 @@ Route::get('/', function () {
         'users' => \App\Models\User::all(),
     ]);
 });
+
+
+Route::get('/{latitude}/{longitude}', function (Request $request, $latitude, $longitude) {
+    $curl = curl_init();
+    $data = ['lat' => $latitude, 'lon' => $longitude, 'appid' => env('APP_WEATHER_KEY')];
+    $url = sprintf("%s?%s", env('APP_WEATHER_URL'), http_build_query($data));
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+    $info = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    if ($info !== 200) {
+        curl_close($curl);
+        return response()->json([
+            'status' => 'failed',
+        ]);
+    }
+
+    curl_close($curl);
+
+    return response()->json([
+        'status' => 'success',
+        'data' => json_decode($result)
+    ]);
+});
--- frontend/src/components/Users.vue
+++ frontend/src/components/Users.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="users">
+  <template v-if="users.length < 1">
+    Waiting....
+  </template>
+  <div v-else class="users">
     <table>
       <thead>
         <tr>
           <th>Name</th>
           <th>Email</th>
           <th>Location</th>
-          <th>Weather</th>
+          <th>Updated</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(user) in users" :key="user.id" @click="showModal(user)">
           <td>{{ user.name }}</td>
           <td>{{ user.email }}</td>
-          <td>{{ user.latitude }}, {{ user.longitude}}</td>
-          <td></td>
+          <td>Lati: {{ user.latitude }}, Logi: {{ user.longitude}}</td>
+          <td>{{ changeFormat(user.updated) }}</td>
         </tr>
       </tbody>
     </table>
@@ -24,6 +27,7 @@
         <h2>{{ selectedUser.name }}'s Weather Report</h2>
         <template v-if="isValid">
           <p>Last Updated: {{ selectedUser.updated }}</p>
+          <p>Location: {{ selectedUser.location }}</p>
           <p>Temperature: {{ selectedUser.temp }}</p>
           <p>Wind Speed: {{ selectedUser.wind }}</p>
           <p>Current Weather: {{ selectedUser.forecast }}</p>
@@ -38,12 +42,12 @@
 </template>
 
 <script>
+import moment from 'moment';
 export default {
   data() {
     return {
-      apiKey: '8e90602fa932c0b2dab3331e65b8afe1',
       users: [],
-      isValid: false,
+      isValid: true,
       showUserModal: false,
       weatherDetails: null,
       selectedUser: null
@@ -53,6 +57,9 @@ export default {
     this.getUsers()
   },
   methods: {
+    changeFormat(val) {
+      return !val ? '' : moment(val).format('D/M/YYYY H:m:s');
+    },
     async getUsers() {
       const response = await (await fetch('http://localhost')).json();
       if (!response?.users?.length) return;
@@ -61,26 +68,36 @@ export default {
       // this.users = 
     },
     async showModal(user) {
-      const currentTime = new Date();
-      const lastUpdatedTime = new Date(user.lastUpdated);
-      const timeDiff = (currentTime - lastUpdatedTime) / (1000 * 60 * 60); // difference in hours
+      let isUpdatable = true;
+      if (user.updated) {
+        isUpdatable = (new Date() - user.updated) / (1000 * 60 * 60) > 1;
+      }
+      
+      this.selectedUser = user;
 
-      let response = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${user.latitude}&lon=${user.longitude}&appid=${this.apiKey}`)).json();
-      if (+response.status === 404) {
-        this.isValid = false;
-      } else {
-        user.temp = response.main.temp;
-        user.humidity = response.main.humidity;
-        user.wind = response.wind.speed;
-        user.forecast = response.weather[0].main;
-        user.detailed = response.weather[0].description;
-        this.isValid = true;
+      if (!isUpdatable) {
+        this.showUserModal = true;
+        return;
       }
 
+      let {status, data} = await (await fetch(`http://localhost/${user.latitude}/${user.longitude}`)).json();
+      if (status === 'failed') {
+        this.isValid = false;
+        this.showUserModal = true;
+        return;
+      }
+
+      user.temp = data.main.temp;
+      user.humidity = data.main.humidity;
+      user.wind = data.wind.speed;
+      user.forecast = data.weather[0].main;
+      user.detailed = data.weather[0].description;
+      user.updated = new Date(+data.dt * 1000);
+      user.location = !data.name ? 'unknown' : `${data.name}${data.name} ${data.sys.country}`;
       this.showUserModal = true;
-      this.selectedUser = user;
     },
     closeModal() {
+      this.isValid = true;
       this.showUserModal = false;
       this.selectedUser = null;
     }
